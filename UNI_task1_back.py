febaafef6117e6d31cc4663ee96a2c299e6b4d36
DHRUVJ2003 <91372908+DHRUVJ2003@users.noreply.github.com>
2022-09-18 14:26:55 +0530

    Add files via upload

---
+++ UNI_task1_back.py
@@ -0,0 +1,23 @@
+def adj(a,b):
+    soln={}
+    for i in range(a,b):
+        q=int(i)
+        r2=0
+        r1=0       
+        if (q==0):
+            soln.update({i:'False'})
+        while(q!=0):
+            r1=int(q%2)
+            q=int(q/2)
+            if ((r1+r2)==2):
+                soln.update({i:'True'})
+                break
+            if (q!=0):
+                r2=r1
+        if (r1!=r2):
+            soln.update({i:'False'})
+    return(soln)
+
+a=int(input())
+b=int(input())
+print(adj(a,b))
